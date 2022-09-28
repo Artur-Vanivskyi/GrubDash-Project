b8ee3fcc36ff0011527fc980832069344c3065e4
Artur <vanivskyi.artur@thinkful.com>
2022-09-28 17:49:44 -0400

    removed comments

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,13 +1,12 @@
 const path = require("path");
 const dishesData = require("../data/dishes-data");
 
-// Use the existing dishes data
+
 const dishes = require(path.resolve("src/data/dishes-data"));
 
-// Use this function to assign ID's when necessary
+
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
 
 function list(req, res, next) {
   res.json({ data: dishes });
